3a90594a0171a6a10aa6470e0e0133854c19cf2d
Sanghyuck Lee <shlee322@elab.kr>
2015-07-30 23:25:43 +0900

    deploy ssh-agent 추가, git add -A 로 변경

--- deploy/after_success.sh
+++ deploy/after_success.sh
@@ -6,14 +6,15 @@ if [ "$BRANCH" != "gh-pages" ]; then
     exit 0
 fi
 
-git add data/
-git add static/
+git add -A data/
+git add -A static/
 git add index.html
 
 git status
 
 openssl aes-256-cbc -K $encrypted_ec37b4905d5e_key -iv $encrypted_ec37b4905d5e_iv -in deploy/deploy_key.pem.enc -out deploy/deploy_key.pem -d
 chmod 600 deploy/deploy_key.pem
+eval $(ssh-agent)
 ssh-add deploy/deploy_key.pem
 
 git commit -m "Deploy"
